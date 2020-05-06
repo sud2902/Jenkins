b9c3e3955770fbcfda693ce0618d0ab68f2be5c2
Richa Gupta <richa@Richas-MacBook-Pro.local>
2020-05-06 19:47:36 +0530

    first commit test

--- firstgui.py
+++ firstgui.py
@@ -1,4 +1,4 @@
-#Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
+# Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
 picture = [
   [0,0,0,1,0,0,0],
   [0,0,1,1,1,0,0],
